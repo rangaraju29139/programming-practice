e68489f2385908d95430de052e988a5f85e884e7
rangaraju29139 <rangaraju29139@gmail.com>
2023-06-29 12:34:38 +0530

    adding the synchronizations issue  assignment

---
+++ os-practice/src/Synchronizations/Adder.java
@@ -0,0 +1,18 @@
+package Synchronizations;
+
+public class Adder implements  Runnable{
+
+    private Count count;
+    public Adder(Count count){
+        this.count=count;
+    }
+    @Override
+    public void run() {
+
+        for(int i=0;i<=10000;i++){
+            count.value+=i;
+            System.out.println(count.value + " printed by Thread "+ Thread.currentThread().getName());
+        }
+
+    }
+}
---
+++ os-practice/src/Synchronizations/Count.java
@@ -0,0 +1,5 @@
+package Synchronizations;
+
+public class Count {
+    int value = 0;
+}

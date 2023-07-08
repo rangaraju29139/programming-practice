15c6b326cecf04a9ac5b4053bdc63aa79cefc183
rangaraju29139 <rangaraju29139@gmail.com>
2023-07-08 10:29:40 +0530

    adder subtractor using atomic variables

---
+++ os-practice/src/Synchronizations/AtomicVariables/Subtractor.java
@@ -0,0 +1,18 @@
+package Synchronizations;
+
+public class Subtractor implements Runnable{
+
+    private Count count;
+
+    public Subtractor(Count count){
+        this.count= count;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0;i<=10000;i++){
+            count.value-=i;
+            System.out.println(count.value + " printed by Thread "+ Thread.currentThread().getName());
+        }
+    }
+}
